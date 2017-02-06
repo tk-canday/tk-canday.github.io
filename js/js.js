7109b3be77183a9801df1145a9261a16ad871586
tk <573216211@qq.com>
2017-02-06 14:00:41 +0800

    基本功能完善

--- js/js.js
+++ js/js.js
@@ -1,19 +1,45 @@
-function b(){
-	h = $(window).height();
-	t = $(document).scrollTop();
-	if(t > h){
-		$('#gotop').show();
-	}else{
-		$('#gotop').hide();
-	}
-}
-$(document).ready(function(e) {
-	b();
-	$('#gotop').click(function(){
-		$(document).scrollTop(0);	
-	})
+// //card 模块图标显示
+$('.talk').mouseenter(function(event) {
+    $(this).next().show();
+});
+$('.linkmore>li >a').mouseleave(function(event) {
+    $('.linkmore>li >img').hide();
 });
 
-$(window).scroll(function(e){
-	b();		
+
+// 底部返回按钮-----------------------------------------------
+
+function b() {
+    var h = $(window).height();
+    var t = $(document).scrollTop();
+    if (t > h) {
+        $('#gotop').show();
+    } else {
+        $('#gotop').hide();
+    }
+}
+
+$(document).ready(function(e) {
+    b();
+    // 返回按钮点击事件
+    $('#gotop').click(function() {
+        clearInterval(timer);
+        var leader = parseInt($(document).scrollTop());
+        var target = 0;
+        var step = (target - leader) / 10;
+        var timer = setInterval(function() {
+            step = step > 0 ? Math.ceil(step) : Math.floor(step);
+            leader = leader > 0 ? leader + step : 0;
+            $(document).scrollTop(leader);
+            if (target === leader) {
+                clearInterval(timer);
+            }
+        }, 17)
+
+
+    })
+});
+
+$(window).scroll(function(e) {
+    b();
 })
